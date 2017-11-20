1ee1da02ca06fd08a63d46da870ced6f9bdf469b
Ernst Plüss <ernst.pluess@ernst-pluess.ch>
2017-11-20 13:49:08 +0100

    Improoved admin layout.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,11 +1,16 @@
 <!doctype html>
-<html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-    <title>Pics Controller</title>
-</head>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
 <script>
     $(function () {
         var socket = io('http://192.168.1.102:3000');
@@ -17,22 +22,28 @@
 </script>
 <style>
     #next-button {
-        width: 100%;
-        margin: 1rem;
         background-color: #957bbe;
-        border-radius: .25rem;
+        border-radius: 1rem;
         color: white;
         text-align: center;
         vertical-align: middle;
         font-family: sans-serif;
         line-height: 10rem;
         font-size: 2rem;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+
+    .border {
+        margin: 1rem;
     }
 </style>
 
 <body>
-    <div id="next-button" class="container-fluid">
-        <h1>Next</h1>
+    <div class="border">
+        <div id="next-button" class="container-fluid">
+            <h1>Next</h1>
+        </div>
     </div>
 
     @foreach ($pics as $pic)
@@ -40,6 +51,8 @@
     @endforeach 
 
     {{ $pics->links() }}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 
 </html> 
